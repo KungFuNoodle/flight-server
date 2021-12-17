e23c3c77d3b2c2eae9a25b2a10c8c7301b5b7399
KungFuNoodle <MiniMonster1123@gmail.com>
2021-12-17 17:07:27 +0200

    Added user table to SQL
    I added a table and user to the SQL

--- migrateSQL.sql
+++ migrateSQL.sql
@@ -5,5 +5,15 @@ create database flight_app;
 use flight_app;
 
 /*Create Tables*/
+Create table userAccounts
+(
+id int primary key auto_increment,
+username varchar(255) not null unique,
+email varchar(255) not null unique,
+password varchar(255) not null
+);
+
 
 /*Seed Tables*/
+Insert into userAccounts(username,email,password)
+values("Admin","adminEmail@something.com","12345")
