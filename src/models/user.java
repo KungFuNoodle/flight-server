8ed862ff448bf0d9cb677dfd697cdd28ba3a139b
KungFuNoodle <MiniMonster1123@gmail.com>
2021-12-17 17:13:21 +0200

    Added user model
    Added the constructor and getters for the database fields

---
+++ src/models/user.java
@@ -0,0 +1,33 @@
+package models;
+
+public class user {
+//veriables
+
+    private String id, username, email, password;
+
+//Constructor
+    public user(String id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+//Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
